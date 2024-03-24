b9684df6588de980a23b85497b28ff7738386e6d
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-03-24 15:46:26 +0530

    moving to nest dir

--- nestjs-sleepr-project/apps/reservations/src/dto/CreateReservationDto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateReservationDto {
-  startDate: Date;
-  endDate: Date;
-  placeId: string;
-  invoiceId: string;
-}
--- nestjs-sleepr-project/libs/common/src/mongodb/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './mongodb.module';
-export * from './abstract.schema';
-export * from './abstract.repository';
