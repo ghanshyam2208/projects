12a1405821bcae581e6697ac7316de3a9bc0d83c
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-03-25 11:40:03 +0530

    impot from common

--- nest-projects/sleepr-project/apps/reservations/src/reservations.module.ts
+++ nest-projects/sleepr-project/apps/reservations/src/reservations.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import {
   ReservationsDocument,
   ReservationsSchema,
 } from './model/reservations.model';
 import { ReservationsRepository } from './reservations.repository';
-import { LoggerModule } from '@app/common';
 
 @Module({
   imports: [
