b0bef5cc90208bd966977642cec5d7575224052c
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-03-29 23:18:31 +0530

    using middleware

--- nest-projects/spotify-clone/src/app.module.ts
+++ nest-projects/spotify-clone/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
@@ -11,6 +12,8 @@ import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware'
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('songs');
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // consumer.apply(LoggerMiddleware).forRoutes('songs');
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
   }
 }
