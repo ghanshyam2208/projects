96045d3e6a7e550749917af7588e68c5ac6ce7fa
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-04-28 17:32:06 +0530

    implemented local strategy

--- nest-projects/sleepr-project-2/apps/auth/src/auth.service.ts
+++ nest-projects/sleepr-project-2/apps/auth/src/auth.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UsersDocument } from './users/users.schema';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: UsersDocument, response: Response) {
+    const tokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+
+    const token = this.jwtService.sign(tokenPayload);
+    response.cookie('Authentication', token, {
+      httpOnly: true, // cookie available for http requests only
+      expires,
+    });
+
+    response.send(user);
   }
 }
--- nest-projects/sleepr-project-2/apps/auth/src/users/users.service.ts
+++ nest-projects/sleepr-project-2/apps/auth/src/users/users.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 import { UsersRepository } from './users.repository';
 import { CreateUserPayload } from './users.validation';
 
@@ -7,6 +8,24 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async createUser(createUserPayload: CreateUserPayload) {
-    this.usersRepository.create(createUserPayload);
+    this.usersRepository.create({
+      ...createUserPayload,
+      password: await bcrypt.hash(createUserPayload.password, 10),
+    });
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Username or password is not valid');
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Username or password is not valid');
+    }
+    return user;
   }
 }
