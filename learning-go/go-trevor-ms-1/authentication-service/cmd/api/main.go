33a15621df9a06ed6183a9e71749019eeddf82cc
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-05-11 03:50:25 +0530

    go

--- learning-go/go-trevor-ms-1/authentication-service/cmd/api/main.go
+++ learning-go/go-trevor-ms-1/authentication-service/cmd/api/main.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "8002"
@@ -16,13 +18,25 @@ type Config struct {
 	Models data.Models
 }
 
+var counts int64
+
 func main() {
 	log.Printf("Starting authentication service on port %s \n", webPort)
+
+	conn := connectToDB()
+
+	if conn == nil {
+		log.Panic("Could not connect to pg")
+		return
+	}
 
 	// TODO connect to DB
 
 	// set up config
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	// define http server
 	srv := &http.Server{
@@ -34,5 +48,47 @@ func main() {
 
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+
+		if err != nil {
+			log.Println("Postgres is not ready yet......")
+			counts++
+		} else {
+			log.Println("Connected to pg")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds.....")
+		time.Sleep(2 * time.Second)
+
+		continue
 	}
 }
