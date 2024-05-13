7539efd6480dfc0eadea5c407833c7190055fe29
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-05-14 03:24:56 +0530

    setting up monorepo setup

---
+++ nest-projects/monorepo-setup/apps/reservations/src/reservations.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsService } from './reservations.service';
+import { DatabaseModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService],
+})
+export class ReservationsModule {}
