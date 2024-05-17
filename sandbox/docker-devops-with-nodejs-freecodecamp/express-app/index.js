887fa75e909b4d0a8f856ce0354255b1b939e8c7
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-05-18 02:49:31 +0530

    connected to mongodb from docker image

--- sandbox/docker-devops-with-nodejs-freecodecamp/express-app/index.js
+++ sandbox/docker-devops-with-nodejs-freecodecamp/express-app/index.js
@@ -1,7 +1,14 @@
 // const express = require("express");
 import express from "express";
+import mongoose, { mongo } from "mongoose";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect("mongodb://root:root@my-node-app-mongo:27017/?authSource=admin")
+  .then(() => console.log("Connected to mongodb"))
+  .catch((e) => console.log("error while connecting to mongodb", e));
 
 app.get("/", (_req, res) => {
   //   res.json({
