69c2e8fc88efda23674afa1e7b8c41f43ebb0474
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-05-18 01:15:16 +0530

    separate prod and dev file for docker compose

--- sandbox/docker-devops-with-nodejs-freecodecamp/express-app/index.js
+++ sandbox/docker-devops-with-nodejs-freecodecamp/express-app/index.js
@@ -4,16 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({
-    success: true,
-    message: "successful response",
-  });
+  //   res.json({
+  //     success: true,
+  //     message: "successful response",
+  //   });
   //   return res.status(200).json({
   //     success: true,
   //     message: "successful response",
   //   });
 
   //   res.status(200).send();
+
+  res.send("<H1>HI There!!</H1>");
 });
 
 app.listen(PORT, () => {
