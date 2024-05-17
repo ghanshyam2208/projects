2b36b1df2e36ca89eb565d93bca03169979e2e56
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-05-18 00:23:56 +0530

    running code from docker

--- sandbox/docker-devops-with-nodejs-freecodecamp/express-app/index.js
+++ sandbox/docker-devops-with-nodejs-freecodecamp/express-app/index.js
@@ -4,16 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  //   res.json({
-  //     success: true,
-  //     message: "successful response",
-  //   });
+  res.json({
+    success: true,
+    message: "successful response",
+  });
   //   return res.status(200).json({
   //     success: true,
   //     message: "successful response",
   //   });
 
-  res.status(200).send();
+  //   res.status(200).send();
 });
 
 app.listen(PORT, () => {
