90dd727b37289a7564e5e17d5a6086f13d178848
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-05-25 23:10:46 +0530

    config to get access values

--- nest-projects/spotify-nestjs-apis2/ms-auth/src/auth.helper.ts
+++ nest-projects/spotify-nestjs-apis2/ms-auth/src/auth.helper.ts
@@ -1,4 +1,4 @@
-import { AuthToken, GetAuthTokenPayload } from 'proto/auth';
+import { GetAuthTokenPayload } from 'proto/auth';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
@@ -8,8 +8,8 @@ export class AuthHelper {
   constructor(private readonly configService: ConfigService) {}
 
   async signJwtToken(payload: GetAuthTokenPayload) {
-    return jwt.sign(payload, 'secretKey', {
-      expiresIn: '1h', // Set the token expiration time
+    return jwt.sign(payload, this.configService.get('ACCESS_TOKEN_SECRET'), {
+      expiresIn: this.configService.get('ACCESS_TOKEN_EXPIRY'), // Set the token expiration time
       algorithm: 'HS256', // Set the algorithm
     });
     // return token;
