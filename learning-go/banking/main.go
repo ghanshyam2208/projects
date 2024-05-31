9133d9c31dcab709dc3a163308669c6b2796607a
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-06-01 00:29:30 +0530

    deleted unrequired code

--- learning-go/banking/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/gnbaviskar/bankingApp/app"
-)
-
-func main() {
-	app.StartApp()
-}
--- learning-go/go-trevor-ms-1/broker-service/broker.service.dockerfile
+++
@@ -1,7 +0,0 @@
-FROM golang:1.22.2-alpine
-
-RUN mkdir /app
-
-COPY brokerApp /app
-
-CMD ["/app/brokerApp"]
