c734ab61f4100327c9467ce3da7d3743f889bb16
ghanshyam2208 <gnbaviskarsyncacc@gmail.com>
2024-06-05 16:11:05 +0530

    implemented dto for get a customer

--- learning-go/banking-app/domain/customer.go
+++ learning-go/banking-app/domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/ghanshyam2208/banking/errs"
+import (
+	"github.com/ghanshyam2208/banking/dto"
+	"github.com/ghanshyam2208/banking/errs"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -14,4 +17,23 @@ type Customer struct {
 type CustomerRepository interface {
 	FindAll(status string) ([]Customer, *errs.AppError)
 	ById(string) (*Customer, *errs.AppError)
+}
+
+func (c Customer) getStatusText() string {
+	statusText := "inactive"
+	if c.Status == "1" {
+		statusText = "active"
+	}
+	return statusText
+}
+
+func (c Customer) CreateCustomerResponse() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateofBirth: c.DateofBirth,
+		Status:      c.getStatusText(),
+	}
 }
--- learning-go/banking-app/service/customerService.go
+++ learning-go/banking-app/service/customerService.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"github.com/ghanshyam2208/banking/domain"
+	"github.com/ghanshyam2208/banking/dto"
 	"github.com/ghanshyam2208/banking/errs"
 )
 
 type CustomerService interface {
 	GetAllCustomer(status string) ([]domain.Customer, *errs.AppError)
-	GetCustomerById(string) (*domain.Customer, *errs.AppError)
+	GetCustomerById(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
@@ -18,8 +19,15 @@ func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer
 	return s.repo.FindAll(status)
 }
 
-func (s DefaultCustomerService) GetCustomerById(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomerById(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	response := c.CreateCustomerResponse()
+
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
